ad6316903b7f3de86cefbb2412713fc6da0a1785
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2025-11-21 05:57:04 +0900

    init: 프로제트 세팅

---
+++ src/test/java/com/jk/amazon2/Amazon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.amazon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amazon2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
