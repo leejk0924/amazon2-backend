9db9183db0c861246e72e0ceaa34687a78eaae9a
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-02-28 05:31:52 +0900

    Feat: 카테고리 목록 조회 동적 정렬 기능 구현
    - update: Pageable Sort 기반 QueryDSL OrderSpecifier 변환 메서드 추가

--- src/main/java/com/jk/amazon2/repository/CategoryRepositoryImpl.java
+++ src/main/java/com/jk/amazon2/repository/CategoryRepositoryImpl.java
@@ -2,15 +2,20 @@ package com.jk.amazon2.repository;
 
 import com.jk.amazon2.controller.dto.CategoryRequest;
 import com.jk.amazon2.entity.Category;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.jk.amazon2.entity.QCategory.category;
@@ -29,6 +34,7 @@ public class CategoryRepositoryImpl implements CategoryQueryRepository{
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
+                .orderBy(getOrderSpecifier(pageable.getSort()))
                 .fetch();
 
         JPAQuery<Long> countQuery = queryFactory
@@ -47,5 +53,17 @@ public class CategoryRepositoryImpl implements CategoryQueryRepository{
 
     private BooleanExpression codeContains(String code) {
         return StringUtils.hasText(code) ? category.code.contains(code) : null;
+    }
+
+    private OrderSpecifier[] getOrderSpecifier(Sort sort) {
+        var orders = new ArrayList<>();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+            PathBuilder pathBuilder = new PathBuilder(category.getType(), category.getMetadata());
+            orders.add(new OrderSpecifier(direction, pathBuilder.get(prop)));
+        });
+        return orders.toArray(OrderSpecifier[]::new);
     }
 }
